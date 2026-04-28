366379606f1b7c82953a2d3cac9de33cac25cd97
Gustavo H.P Andrade <168314032+GustavoAndradeBr@users.noreply.github.com>
2026-04-28 10:54:15 -0300

    Update main.jsx for StrictMode and routing
    Refactor main.jsx to use createRoot and conditional rendering.

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import Cartao from "./pages/Cartao.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+const path = window.location.pathname;
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    {path === "/cartao" ? <Cartao /> : <App />}
+  </StrictMode>
+);
